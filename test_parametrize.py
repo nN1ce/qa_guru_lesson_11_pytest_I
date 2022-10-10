cc468f031b0ddca653f78f9bf2200f3fa4712bae
skhomuti <skhomuti@gmail.com>
2022-10-10 23:03:09 +0400

    Add lesson materials

--- test_parametrize.py
+++ test_parametrize.py
@@ -1,8 +1,62 @@
 from dataclasses import dataclass
 
+import pytest
 
-def test_with_param():
+
+@pytest.mark.parametrize(["browser", "test_user"],
+                         [("Chrome", 100), ("Firefox", 200)],
+                         ids=["Chrome with business account", "Firefox with personal account"],
+                         )
+def test_with_param(browser, test_user):
     pass
+
+
+@pytest.mark.parametrize("browser", ["Chrome", "Firefox"])
+@pytest.mark.parametrize("test_user", [100, 200], ids=["business account", "personal account"])
+def test_with_param2(browser, test_user):
+    pass
+
+
+@pytest.mark.parametrize("browser", [pytest.param("Firefox", marks=[pytest.mark.skip("Firefox tests broken")]),
+                                     pytest.param("Chrome")])
+@pytest.mark.parametrize("test_user", [100, 200], ids=["business account", "personal account"])
+def test_with_param3(browser, test_user):
+    pass
+
+
+@pytest.fixture(params=["Chrome", "Firefox"])
+def browser(request):
+    assert request.param in ["Chrome", "Firefox"]
+    return {"browser_name": request.param}
+
+
+@pytest.fixture(params=[100])
+def test_user(request):
+    return request.param
+
+
+def test_with_parametrized_fixture(browser, test_user):
+    pass
+
+
+@pytest.fixture(params=[])
+def account(request):
+    pass
+
+
+@pytest.mark.parametrize("browser", ["Chrome"], indirect=True)
+def test_with_indirect_parametrization(browser):
+    assert browser == "Chrome"
+
+
+personal = pytest.mark.parametrize("account", ["personal"], indirect=True)
+
+
+@personal
+def test_with_account(account):
+    pass
+
+
 
 
 @dataclass
@@ -12,10 +66,18 @@ class User:
     age: int
     description: str
 
+    # def __repr__(self):
+    #     return f"{self.name} ({self.id})"
+
 
 user1 = User(id=1, name="Mario", age=32, description="something " * 10)
 user2 = User(id=2, name="Wario", age=62, description="else " * 10)
 
 
-def test_users():
-    pass
+def show_user(user):
+    return f"{user.name} ({user.id})"
+
+
+@pytest.mark.parametrize("user", [user1, user2], ids=show_user)
+def test_users(user):
+    print()
--- test_simple.py
+++ test_simple.py
@@ -1,19 +1,51 @@
+import os
+
 import pytest
 import time
 
 
 @pytest.fixture()
 def browser():
+    """Какой-нибудь браузер - chrome or firefox"""
     time.sleep(1)
 
 
+@pytest.mark.slow
 def test_first(browser):
     time.sleep(1)
 
 
+@pytest.mark.fast
 def test_second():
     time.sleep(1)
 
 
+@pytest.mark.fast
+@pytest.mark.skip(reason="TASK-123 Тест нестабильный потому что тестовые данные приходят неверные")
 def test_third():
-    time.sleep(1)
+    a = 25
+    b = 150
+    assert a == b
+
+
+@pytest.mark.skipif("os.getenv('OS') == 'Linux'", reason="Something")
+def test_skipped(browser):
+    if os.getenv("OS") == "linux":
+        pytest.skip("reason")
+    if browser == 'Safari':
+        pytest.skip("Safari doesn't supported")
+
+
+@pytest.mark.xfail(reason="some reason")
+def test_xfailed():
+    assert False
+    time.sleep(5)
+    try:
+        assert 1 == 5
+    except AssertionError:
+        pytest.xfail()
+
+
+@pytest.mark.usefixtures("browser")
+def test_with_browser():
+    pass
