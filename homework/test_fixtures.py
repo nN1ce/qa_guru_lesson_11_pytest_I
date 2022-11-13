a5e7f4f142f8324ce2e2ccbe6e00a53f2e3fac76
nN1ce <nicevector@yandex.ru>
2022-11-13 22:44:17 +0300

    add_test_fixture_hw

--- homework/test_fixtures.py
+++ homework/test_fixtures.py
@@ -2,10 +2,28 @@
 Сделайте разные фикстуры для каждого теста
 """
 
+import pytest
+from selene.support.shared import browser
 
-def test_github_desktop():
-    pass
+@pytest.fixture(scope='function')
+def desktop():
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
 
-def test_github_mobile():
-    pass
+@pytest.fixture(scope='function')
+def mobile():
+    browser.config.window_width = 800
+    browser.config.window_height = 600
+
+
+
+def test_github_signin_desktop(desktop):
+    browser.open('/')
+    browser.element(".HeaderMenu-link--sign-in").click()
+
+
+def test_github_signin_mobile(mobile):
+    browser.open('/')
+    browser.element(".Button-label").click()
+    browser.element(".HeaderMenu-link--sign-in").click()
